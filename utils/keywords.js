b18eeb02ae9821fec33b9e7d6886f83f3b33c1b7
Leo Oliveira <qwerty124563@gmail.com>
2021-03-05 12:23:42 +0000

    Add scopes of the bot and keywords to detect

---
+++ utils/keywords.js
@@ -0,0 +1,7 @@
+module.exports = {
+    IDENTIFY_ME: ['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    SHUTDOWN: ['shutdown'],
+    WHATS_USER_NAME: ['what is my name', 'who am i'],
+    SET_USER_NAME: ['call me (.*)', 'my name is (.*)'],
+    GREETINGS: ['hello', 'hi'],
+};
---
+++ utils/scopes.js
@@ -0,0 +1,7 @@
+const scopes = [
+    'direct_message',
+    'direct_mention',
+    'mention',
+];
+
+module.exports = scopes.join(',');
