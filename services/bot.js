bbab92c4922ce85688ca53c00a1ed02dcbac078d
Leo Oliveira <qwerty124563@gmail.com>
2021-03-05 12:32:27 +0000

    Fix some code

--- services/bot.js
+++ services/bot.js
@@ -64,59 +64,64 @@ controller.hears(keywords.WHATS_USER_NAME, scopes, function (bot, message) {
         }
 
         bot.startConversation(message, (err, convo) => {
-            if (!err) {
-                convo.say('I do not know your name yet!');
-                convo.ask('What should I call you?', (response, convo) => {
-
-                    convo.ask(`You want me to call you ${response.text} ?`, [
-                        {
-                            pattern: 'yes',
-                            callback: (response, convo) => {
-                                // since no further messages are queued after this,
-                                // the conversation will end naturally with status == 'completed'
-                                convo.next();
-                            }
-                        },
-                        {
-                            pattern: 'no',
-                            callback: (response, convo) => convo.stop(),
-                        },
-                        {
-                            default: true,
-                            callback: (response, convo) => {
-                                convo.repeat();
-                                convo.next();
-                            }
-                        }
-                    ]);
-
-                    convo.next();
-                    // store the results in a field called nickname
-                }, {'key': 'nickname'});
-
-                convo.on('end', (convo) => {
-                    if (convo.status === 'completed') {
-                        bot.reply(message, 'OK! I will update my dossier...');
-
-                        controller.storage.users.get(message.user, (err, user) => {
-                            if (!user) {
-                                user = {
-                                    id: message.user,
-                                };
-                            }
-                            user.name = convo.extractResponse('nickname');
-                            controller.storage.users.save(user, () => {
-                                bot.reply(message, `Got it. I will call you ${user.name} from now on.`);
-                            });
-                        });
-
-                        return;
-                    }
-                    // this happens if the conversation ended prematurely for some reason
-                    bot.reply(message, 'OK, nevermind!');
-
-                });
+            if (err) {
+                bot.botkit.log('Cant start a conversation.', err);
+                return;
             }
+
+            convo.say('I do not know your name yet!');
+            convo.ask('What should I call you?', (response, convo) => {
+
+                convo.ask(`You want me to call you ${response.text} ?`, [
+                    {
+                        pattern: 'yes',
+                        callback: (response, convo) => {
+                            // since no further messages are queued after this,
+                            // the conversation will end naturally with status == 'completed'
+                            convo.next();
+                        }
+                    },
+                    {
+                        pattern: 'no',
+                        callback: (response, convo) => convo.stop(),
+                    },
+                    {
+                        default: true,
+                        callback: (response, convo) => {
+                            convo.repeat();
+                            convo.next();
+                        }
+                    }
+                ]);
+
+                convo.next();
+                // store the results in a field called nickname
+            }, {'key': 'nickname'});
+
+            convo.on('end', (convo) => {
+                if (convo.status === 'completed') {
+                    bot.reply(message, 'OK! I will update my dossier...');
+
+                    controller.storage.users.get(message.user, (err, user) => {
+                        if (!user) {
+                            user = {
+                                id: message.user,
+                            };
+                        }
+
+                        user.name = convo.extractResponse('nickname');
+                        controller.storage.users.save(user, () => {
+                            bot.reply(message, `I will call you ${user.name} from now on.`);
+                        });
+                    });
+
+                    return;
+                }
+                // conversation ended
+                bot.reply(message, 'bye!');
+
+            });
+
         });
 
     });
