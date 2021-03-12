96e8c328f15d852ad330b130593049b28799d719
Leo Oliveira <qwerty124563@gmail.com>
2021-03-12 11:34:48 +0000

    add makeTaskRequest

--- services/bot/actions/tasks.js
+++ services/bot/actions/tasks.js
@@ -2,29 +2,42 @@ const helpers = require('../../../utils/helpers');
 const consumer = require('../../consumer');
 const axios = require('axios');
 
+
+let makeTaskRequest = (params = {})  => {
+
+    let data = {
+        ...{
+            filter: "1=1",
+            code: "",
+            jtStartIndex: 0,
+            jtPageSize: 10,
+            jtSorting: helpers.buildSorting('ID', 'int'),
+            token: process.env.API_TOKEN
+        },
+        ...params
+    };
+
+    return axios.post('/RequestFilteredList', data);
+};
+
 module.exports = {
 
     //get my first 5 tasks of type 16
     GET_TASKS_BY_TYPE: (bot, message) => {
         let numberTasks = parseInt(message.match[1]);
-        let type = parseInt(message.match[2])
+        let type = parseInt(message.match[2]);
 
         bot.reply(message, `Obtendo ${numberTasks} tarefas of type ${type}`);
 
-        axios.post('/RequestFilteredList', {
-            filter: "__int__tipo=24",
-            code: "",
-            jtStartIndex: 0,
-            jtPageSize: numberTasks,
-            jtSorting: helpers.buildSorting('ID', 'int'),
-            token: process.env.API_TOKEN
-        }).then(response => {
+
+        makeTaskRequest({ jtPageSize: numberTasks }).then(response => {
             let messageTasks = helpers.buildResponseTasks(response.data.d.Records, [
                 'Id', 'Reference', 'Description', 'Status'
             ]);
 
             bot.reply(message, messageTasks);
         }).catch(console.log);
+
     },
 
     GET_TASKS: (bot, message) => {
@@ -34,14 +47,7 @@ module.exports = {
 
         bot.reply(message, `Obtendo ${numberTasks} tarefas...`);
 
-        axios.post('/RequestFilteredList', {
-            filter: "1=1",
-            code: "",
-            jtStartIndex: 0,
-            jtPageSize: numberTasks,
-            jtSorting: helpers.buildSorting('ID', 'int'),
-            token: process.env.API_TOKEN
-        }).then(response => {
+        makeTaskRequest({ filter: "__int__tipo=24", jtPageSize: numberTasks }).then(response => {
             let messageTasks = helpers.buildResponseTasks(response.data.d.Records, [
                 'Id', 'Reference', 'Description', 'Status'
             ]);
