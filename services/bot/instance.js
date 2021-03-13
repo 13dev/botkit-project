5b07084fea1977b35835519781d4b9538fe467ec
13dev <qwerty124563@gmail.com>
2021-03-13 19:39:31 +0000

    Update botkit to version ^4

--- services/bot/instance.js
+++ services/bot/instance.js
@@ -1,15 +1,28 @@
-const botkit = require('botkit');
-const controller = botkit.slackbot({
-    debug: true,
-    scopes: [
-        'direct_message',
-        'direct_mention',
-        'mention',
-    ],
+// const botkit = require('botkit');
+// const controller = botkit.slackbot({
+//     debug: true,
+//     scopes: [
+//         'direct_message',
+//         'direct_mention',
+//         'mention',
+//     ],
+// });
+//
+// controller.spawn({
+//     token: process.env.TOKEN
+// }).startRTM();
+
+
+const { Botkit } = require('botkit');
+const { SlackAdapter } = require('botbuilder-adapter-slack');
+
+let adapter = new SlackAdapter({
+    botToken: process.env.TOKEN,
 });
 
-controller.spawn({
-    token: process.env.TOKEN
-}).startRTM();
+let controller = new Botkit({
+    adapter: adapter,
+});
+
 
 module.exports = controller;
